27c1c7e1358701cf3e8e092310f65dfd345649f4
Vitali Pantsialei <bitvitalby@gmail.com>
2016-07-27 13:26:31 +0300

    Add schemas

---
+++ Task2RSS/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task2RSS
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
